eb0f3e7d29cf098711fafbd9941cf7d12560dc73
fourierz517 <83400846+fourierz517@users.noreply.github.com>
2022-03-13 23:28:08 -0700

    Update plot_yahoo_finance.py

--- plot_yahoo_finance.py
+++ plot_yahoo_finance.py
@@ -11,6 +11,7 @@ I assume no responsibility for, and offer no warranties or representations regar
 
 #To add: labels of the plots 
 """
+import time
 import datetime
 
 import requests
@@ -19,7 +20,8 @@ import pandas as pd
 
 def retrieve_ticker_history(ticker : str):
     #ticker = 'NVDA'
-    url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + ticker.upper() + '?formatted=true&crumb=FIOmmg93x1j&lang=en-US&region=US&includeAdjustedClose=true&interval=1d&period1=916963200&period2=1646870400&events=capitalGain%7Cdiv%7Csplit&useYfid=true&corsDomain=finance.yahoo.com'
+    curr_time = str(int(time.time()))
+    url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + ticker.upper() + '?formatted=true&crumb=FIOmmg93x1j&lang=en-US&region=US&includeAdjustedClose=true&interval=1d&period1=916963200&period2=' + curr_time + '&events=capitalGain%7Cdiv%7Csplit&useYfid=true&corsDomain=finance.yahoo.com'
     web_content = requests.get(url, headers = {'User-agent': 'Mozilla/5.0'})
     ticker_info = web_content.json()
     if ticker_info['chart']['error']:
@@ -30,10 +32,11 @@ def retrieve_ticker_history(ticker : str):
     ticker_df['Timestamp'] = ticker_info['chart']['result'][0]['timestamp']
     ticker_df['Date'] = ticker_df['Timestamp'].apply(lambda d: datetime.datetime.utcfromtimestamp(d).strftime('%Y-%m-%d'))
     ticker_df = ticker_df.set_index('Date')
+    ticker_df.drop(ticker.df.tail(1).index,inplace=True)
     ticker_df['close'].iloc[-500:].plot(figsize=(10,6))
-    ticker_df['close'].rolling(window = 250).mean().iloc[-500:].plot()
-    ticker_df['close'].rolling(window = 50).mean().iloc[-500:].plot()
-    ticker_df['close'].rolling(window = 20).mean().iloc[-500:].plot()
+    ticker_df['close'].rolling(window=250).mean().iloc[-500:].plot()
+    ticker_df['close'].rolling(window=50).mean().iloc[-500:].plot()
+    ticker_df['close'].rolling(window=20).mean().iloc[-500:].plot()
     print("Currently trading at: " + str(ticker_info['chart']['result'][0]['meta']["regularMarketPrice"]))
 if __name__ == '__main__':
     ticker = input('Enter a ticker: ')
